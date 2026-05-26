153d831e577f57cf5ea438f1974a6be53c836219
mq2vin <mcatan@iut.univ-paris8.fr>
2026-05-26 16:11:37 +0200

    Init

---
+++ src/main/java/org/universite_Paris8/iut/tp2026/gr20/jeuQuizz/App.java
@@ -0,0 +1,10 @@
+package org.universite_Paris8.iut.tp2026.gr20.jeuQuizz;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
